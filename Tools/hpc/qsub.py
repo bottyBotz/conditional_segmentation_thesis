9842b3df033d6e4089da04d9625017b90c2f4749
Dan <67231425+bottyBotz@users.noreply.github.com>
2022-05-27 10:42:26 +0100

    comments

--- Tools/hpc/qsub.py
+++ Tools/hpc/qsub.py
@@ -1,17 +1,25 @@
+###
+#
+
+
+
 import os
 import sys
 from glob import glob
+#glob lets you iterate through across all text files in a folder or any file in a folder. Stores them as a list.
 
-pattern = sys.argv[1]
-scripts = glob(pattern, recursive=True)
+pattern = sys.argv[1] #Get the first argument after the filename
+scripts = glob(pattern, recursive=True) #
 
 print('Going to submit following jobs:')
 [print(i) for i in scripts]
+
 
 def run_script(script_path):
     assert os.path.exists(script_path), f"file {script_path} not exists"
     os.system(f"qsub {script_path}")
 
+#Loop thorugh scripts wishing to be run.
 while(1):
     ans = input("Sure to do that? [Y/N]")
     if ans == "Y":
