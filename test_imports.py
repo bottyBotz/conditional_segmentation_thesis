849e93ea6d68c567fea7cff71ec1b97262c784c0
bottyBotz <dmefting@gmail.com>
2022-06-22 08:55:58 -0200

    Initial Commit, test environment

---
+++ test_imports.py
@@ -0,0 +1,17 @@
+import numpy as np
+import torch
+import nibabel
+import scipy
+import matplotlib
+import seaborn
+import pandas
+#import opencv-python #https://stackoverflow.com/questions/51853018/how-do-i-install-opencv-using-pip
+import torchvision
+import pystrum
+import ujson
+import tensorboard
+
+a = np.zeros((1, 1))
+print(a)
+
+
