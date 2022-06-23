1d52b65f0ce481b8879f3576689fed585a69ab74
bottyBotz <dmefting@gmail.com>
2022-06-23 06:38:22 -0200

    config

--- scripts/condi-seg/weiss/segModCT_cv0.sh
+++ scripts/condi-seg/weiss/segModCT_cv0.sh
@@ -3,25 +3,27 @@
 #$ -l gpu=true
 #$ -l h_rt=96:0:0
 #$ -j y
-#$ -N segMCTCV1
+#$ -N segMCTCV0
 #$ -cwd
 hostname
 date
 python3 -u train.py \
 --project CBCTUnetSeg \
---exp_name hpc.04-1.segModeCTCV1 \
---data_path /home/bot/thesis/Data/others/deepRegData/fullResCropIntensityClip_resampled \
---batch_size 8 \
+--exp_name hpc.04-0.segModeCTCV0 \
+--data_path /home/bot/thesis/Data/others/deepRegData/fullResCropIntensityClip_resampled_overfit \
+--batch_size 32 \
 --input_mode ct \
 --inc 1 \
 --outc 2 \
---cv 1 \
+--cv 0 \
 --input_shape 64 101 91 \
 --lr 1e-5 \
 --affine_scale 0.15 \
---save_frequency 10 \
---num_epochs 25 \
+--save_frequency 100 \
+--num_epochs 10000 \
 --w_dce 1.0 \
 --using_HPC 1 \
 --nc_initial 16 \
 --two_stage_sampling 0 
+                   
+                   
