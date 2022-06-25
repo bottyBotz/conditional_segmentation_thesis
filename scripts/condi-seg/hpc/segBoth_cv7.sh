65f700a32f00566500658a4a19433c4436fe77b0
Daniel EFTING <defting@comic2.cs.ucl.ac.uk>
2022-06-25 13:22:10 +0100

    use hpc = 1

---
+++ scripts/condi-seg/hpc/segBoth_cv7.sh
@@ -0,0 +1,30 @@
+#$ -S /bin/bash
+#$ -l tmem=11G
+#$ -l gpu=true
+#$ -l h_rt=96:0:0
+#$ -j y
+#$ -N segMCTCV0
+#$ -cwd
+hostname
+date
+python3 -u train.py \
+--project CBCTUnetSeg \
+--exp_name segModeBothCV7 \
+--data_path ../../../raid/candi/daniel/Data/others/deepRegData/fullResCropIntensityClip_resampled \
+--batch_size 16 \
+--input_mode both \
+--inc 2 \
+--outc 2 \
+--gpu 1 \
+--cv 7 \
+--input_shape 64 101 91 \
+--lr 1e-5 \
+--affine_scale 0.15 \
+--save_frequency 100 \
+--num_epochs 10000 \
+--w_dce 1.0 \
+--using_HPC 1 \
+--nc_initial 16 \
+--two_stage_sampling 0 \
+                   
+                   
