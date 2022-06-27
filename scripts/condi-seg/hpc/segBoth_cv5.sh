17ca174252e082895bc53f424a694ecb0fd557b6
Daniel EFTING <defting@beaker.cs.ucl.ac.uk>
2022-06-27 11:53:04 +0100

    Updates

--- scripts/condi-seg/hpc/segBoth_cv5.sh
+++ scripts/condi-seg/hpc/segBoth_cv5.sh
@@ -1,7 +1,7 @@
 #$ -S /bin/bash
 #$ -l tmem=11G
 #$ -l gpu=true
-#$ -l h_rt=96:0:0
+#$ -l h_rt=300:0:0
 #$ -j y
 #$ -N segBothCV5
 #$ -cwd
@@ -20,7 +20,7 @@ python3 -u train.py \
 --lr 1e-5 \
 --affine_scale 0.15 \
 --save_frequency 100 \
---num_epochs 30000 \
+--num_epochs 50000 \
 --w_dce 1.0 \
 --using_HPC 1 \
 --nc_initial 16 \
