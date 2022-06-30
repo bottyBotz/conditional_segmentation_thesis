bfe9173ed01646a8bd06a9a1c2bb95b48e53bb5e
Daniel EFTING <defting@beaker.cs.ucl.ac.uk>
2022-06-30 11:53:50 +0100

    reorganize submission scripts

---
+++ scripts/condi-seg/hpc/test.sh
@@ -0,0 +1,26 @@
+#$ -S /bin/bash
+#$ -l tmem=11G
+#$ -l gpu=true
+#$ -l h_rt=10:0:0
+#$ -j y
+#$ -N CS_cv0_nc16
+#$ -cwd
+hostname
+date
+source /share/apps/source_files/python/python-3.9.5.source
+source /share/apps/source_files/cuda/cuda-11.0.source
+python3 -u train.py \
+--project ConditionalSeg \
+--exp_name CondisegCBCT_cv0_nc16 \
+--data_path ./Data/fullResCropIntensityClip_resampled \
+--batch_size 8 \
+--cv 0 \
+--input_shape 64 101 91 \
+--lr 3e-5 \
+--affine_scale 0.15 \
+--save_frequency 500 \
+--num_epochs 10 \
+--w_dce 1.0 \
+--using_HPC 1 \
+--nc_initial 16
+                   
