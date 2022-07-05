9843d054aa5d1363a4888319773192f80fdede72
bottyBotz <dmefting@gmail.com>
2022-07-05 13:22:00 +0000

    Initial Commit

---
+++ scripts/condi-seg/weiss/condiSegReversed_cv0_test.sh
@@ -0,0 +1,25 @@
+#$ -S /bin/bash
+#$ -l tmem=11G
+#$ -l gpu=true
+#$ -l h_rt=300:0:0
+#$ -j y
+#$ -N CS_cv1_nc16
+#$ -cwd
+hostname
+date
+python3 -u train.py \
+--project ConditionalSegReversed \
+--exp_name CondisegCBCT_cv0_nc16 \
+--data_path ../../../raid/candi/daniel/Data/others/deepRegData/fullResCropIntensityClip_resampled \
+--batch_size 8 \
+--cv 0 \
+--input_shape 64 101 91 \
+--lr 3e-5 \
+--affine_scale 0.15 \
+--gpu 1 \
+--save_frequency 5 \
+--num_epochs 10 \
+--w_dce 1.0 \
+--using_HPC 0 \
+--nc_initial 16
+                   
