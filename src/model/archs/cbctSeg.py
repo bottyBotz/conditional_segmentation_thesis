be91b72b1ac22999e7971ca079083df8824fe84d
bottyBotz <dmefting@gmail.com>
2022-07-08 12:46:54 +0000

    cleanup

--- src/model/archs/cbctSeg.py
+++ src/model/archs/cbctSeg.py
@@ -34,6 +34,7 @@ class cbctSeg(BaseArch):
         return net.cuda()
 
     def set_dataloader(self):
+        """Sets Training, Validation, and Holdout Test Set for the cross validation fold"""        
         self.train_set = dataloaders.CBCTData(config=self.config, phase='train')
         self.train_loader = DataLoader(
             self.train_set, 
@@ -50,6 +51,18 @@ class cbctSeg(BaseArch):
         print('>>> Holdout set ready.')
 
     def get_input(self, input_dict, aug=True):
+        """_summary_
+
+        Args:
+            input_dict (_type_): _description_
+            aug (bool, optional): _description_. Defaults to True.
+
+        Raises:
+            NotImplementedError: _description_
+
+        Returns:
+            _type_: _description_
+        """        
         fx_img, mv_img = input_dict['fx_img'].cuda(), input_dict['mv_img'].cuda()  # [batch, 1, x, y, z], image
         fx_seg, mv_seg = input_dict['fx_seg'].cuda(), input_dict['mv_seg'].cuda()  # label
         fx_seg, mv_seg = fx_seg[:, 0, ...], mv_seg[:, 0, ...]
@@ -97,6 +110,10 @@ class cbctSeg(BaseArch):
 
 
     def train(self):
+        """
+        Training and validation loop for the model. Writes to tensorboard and logs directory for the current folds training and validation. 
+        Performs inference on the holdout set after training.
+        """        
         self.save_configure()
         optimizer = optim.Adam(self.net.parameters(), lr=self.config.lr, weight_decay=1e-6)
         log_dir = f"./logs/{self.config.project}"
@@ -108,18 +125,18 @@ class cbctSeg(BaseArch):
                 self.train_mode()
                 print('-' * 10, f'Train epoch_{self.epoch}', '-' * 10)
                 for self.step, input_dict in enumerate(self.train_loader):
-                    input_tensor, gt_seg = self.get_input(input_dict)
+                    input_tensor, gt_seg = self.get_input(input_dict) # [batch, c, x, y, z], cbct_seg
 
-                    optimizer.zero_grad()
+                    optimizer.zero_grad() # clear gradients
 
-                    pred_seg = self.net(input_tensor)
+                    pred_seg = self.net(input_tensor) # forward pass
 
-                    global_loss = self.loss(pred_seg, gt_seg)
+                    global_loss = self.loss(pred_seg, gt_seg) # compute loss
                     global_loss.backward() #backward pass
                     optimizer.step() #Gradient Descent
                 
                 self.writer.add_scalar(f"{self.config.project}/{self.config.exp_name}/Loss/train", global_loss, self.epoch) #Write Loss for Epoch to Tensorboard
-                f.write(f"{self.config.project},{self.config.exp_name},{self.config.cv},'train',{self.epoch},{global_loss},'loss'\n")
+                f.write(f"{self.config.project},{self.config.exp_name},{self.config.cv},'train',{self.epoch},{global_loss},'loss'\n") #Write Loss for Epoch to Log File
                 
                 #Save the model at periodic frequencies
                 if self.epoch % self.config.save_frequency == 0:
@@ -142,6 +159,7 @@ class cbctSeg(BaseArch):
         L_All = 0
         Info = f'step {self.step}'
         
+        #Weighted Dice
         if self.config.w_dce != 0:
             L_dice = 0
             for label_idx in range(pred_seg.shape[1]):
@@ -153,6 +171,7 @@ class cbctSeg(BaseArch):
             L_All += L_dice
             Info += f', Loss_dice: {L_dice:.3f}'
             
+        #Binary Cross Entropy
         if self.config.w_bce != 0:
             L_BCE = loss.wBCE(pred_seg, fx_seg, weights=self.config.class_weights)
             L_All += L_BCE
@@ -163,18 +182,18 @@ class cbctSeg(BaseArch):
         print(Info)
         return L_All
 
-    @torch.no_grad()
+    @torch.no_grad() #No Gradient Computation for validation step
     def validation(self, f = None):
-        self.val_mode()
-        visualization_path = os.path.join(self.log_dir, f'{self.config.exp_name}-vis-in-val')
-        os.makedirs(visualization_path, exist_ok=True)
+        self.val_mode() #Set model to validation mode
+        visualization_path = os.path.join(self.log_dir, f'{self.config.exp_name}-vis-in-val') #Path to save visualization
+        os.makedirs(visualization_path, exist_ok=True) #Create directory if it doesn't exist
 
         res = []
         #Iterate through validation dataloader
         for idx, input_dict in enumerate(self.val_loader):
             input_tensor, gt_seg = self.get_input(input_dict, aug=False) #No augmentation for validation
             pred_seg = self.net(input_tensor) #Get Predicted Segmentation from trained model
-            subject = input_dict['subject']
+            subject = input_dict['subject'] 
 
             #Iterate through the labels for rectum, bladder to calculate binary_dice metric
             for label_idx in range(pred_seg.shape[1]):
@@ -187,8 +206,8 @@ class cbctSeg(BaseArch):
         mean, std = torch.mean(res), torch.std(res) #Aggregate binary_dice across all subjects in validation set
         self.writer.add_scalar(f"{self.config.project}/{self.config.exp_name}/Dice_Mean/validation", mean, self.epoch) #Write Dice for Epoch to Tensorboard
         self.writer.add_scalar(f"{self.config.project}/{self.config.exp_name}/Dice_Std/validation", std, self.epoch) #Write Dice for Epoch to Tensorboard
-        f.write(f"{self.config.project},{self.config.exp_name},{self.config.cv},'val',{self.epoch},{mean},'dice_mean'\n")
-        f.write(f"{self.config.project},{self.config.exp_name},{self.config.cv},'val',{self.epoch},{std},'dice_std'\n")
+        f.write(f"{self.config.project},{self.config.exp_name},{self.config.cv},'val',{self.epoch},{mean},'dice_mean'\n") #Write Dice for Epoch to Log File
+        f.write(f"{self.config.project},{self.config.exp_name},{self.config.cv},'val',{self.epoch},{std},'dice_std'\n") #Write Dice for Epoch to Log File
        
 
         #Save the best model as it's performance on the validation set
@@ -199,11 +218,11 @@ class cbctSeg(BaseArch):
         print('Dice:', mean, std)
 
 
-    @torch.no_grad()
+    @torch.no_grad() #No Gradient Computation for inference step
     def inference(self):
-        self.test_mode()
-        visualization_path = os.path.join(self.log_dir, f'{self.config.exp_name}-vis-{self.epoch}')
-        os.makedirs(visualization_path, exist_ok=True)
+        self.test_mode() #Set model to test mode
+        visualization_path = os.path.join(self.log_dir, f'{self.config.exp_name}-vis-{self.epoch}') #Path to save visualization
+        os.makedirs(visualization_path, exist_ok=True) #Create directory if it doesn't exist
 
         results = {
             'dice': [],
@@ -218,7 +237,7 @@ class cbctSeg(BaseArch):
             
             subject = input_dict['subject']
 
-
+            #Iterate through the labels for rectum and bladder, computing dice on each
             for label_idx in range(gt_seg.shape[1]):
                 aft_dice = loss.binary_dice(pred_seg[:, label_idx, ...], gt_seg[:, label_idx, ...]).cpu().numpy()
 
@@ -244,6 +263,7 @@ class cbctSeg(BaseArch):
         #self.writer.add_scalar(f"{self.config.project}/{self.config.exp_name}/aft_dice_mean/inference", mean, self.epoch) #Write Dice for Epoch to Tensorboard
         #self.writer.add_scalar(f"{self.config.project}/{self.config.exp_name}/aft_dice_std/inference", std, self.epoch) #Write Dice for Epoch to Tensorboard
 
+        #Write resuults on test set to results.pkl file.
         with open(os.path.join(self.log_dir, 'results.pkl'), 'wb') as f:
             pkl.dump(results, f)
 
