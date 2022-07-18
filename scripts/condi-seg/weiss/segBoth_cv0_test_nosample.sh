e8b662c77c62b8f9f5b3ef81af23203b9eb64a07
bottyBotz <defting@gmail.com>
2022-07-18 07:52:48 -0200

    change to no sample test

--- scripts/condi-seg/weiss/segBoth_cv0_test_nosample.sh
+++ scripts/condi-seg/weiss/segBoth_cv0_test_nosample.sh
@@ -10,8 +10,8 @@ date
 python3 -u train.py \
 --project CBCTUnetSeg \
 --exp_name segModeBothCV0 \
---data_path ../../../raid/candi/daniel/Data/others/deepRegData/fullResCropIntensityClip_resampled \
---batch_size 16 \
+--data_path ../../../../raid/candi/daniel/Data/others/deepRegData/fullResCropIntensityClip_resampled \
+--batch_size 2 \
 --input_mode both \
 --inc 2 \
 --outc 2 \
@@ -21,10 +21,10 @@ python3 -u train.py \
 --lr 1e-5 \
 --affine_scale 0.15 \
 --save_frequency 100 \
---num_epochs 10000 \
+--num_epochs 10 \
 --w_dce 1.0 \
 --using_HPC 0 \
 --nc_initial 16 \
---two_stage_sampling 1 \
+--two_stage_sampling 0 \
                    
                    
