69bcfe4bcaef8fee70f4ac4ee99bfb571a0a6973
bottyBotz <dmefting@gmail.com>
2022-07-19 13:58:18 +0100

    Initial Commit

---
+++ scripts/condi-seg/hpc/no_affine/segBoth/segBoth_cv4.sh
@@ -0,0 +1,31 @@
+#$ -S /bin/bash
+#$ -l tmem=11G
+#$ -l gpu=true
+#$ -l h_rt=75:0:0
+#$ -j y
+#$ -N segBothCV4
+#$ -cwd
+hostname
+date
+source /share/apps/source_files/python/python-3.9.5.source
+source /share/apps/source_files/cuda/cuda-11.0.source
+python3 -u train.py \
+--project CBCTUnetSeg \
+--exp_name noaffine_segModeBothCV4 \
+--data_path ./Data/fullResCropIntensityClip_resampled \
+--batch_size 8 \
+--input_mode both \
+--inc 2 \
+--outc 2 \
+--cv 4 \
+--input_shape 64 101 91 \
+--lr 1e-5 \
+--affine_scale 0.0 \
+--save_frequency 1000 \
+--num_epochs 20000 \
+--w_dce 1.0 \
+--using_HPC 1 \
+--nc_initial 16 \
+--two_stage_sampling 0 \
+                   
+                   
---
+++ scripts/condi-seg/hpc/no_affine/segCBCT/segCBCT_cv6.sh
@@ -0,0 +1,31 @@
+#$ -S /bin/bash
+#$ -l tmem=11G
+#$ -l gpu=true
+#$ -l h_rt=75:0:0
+#$ -j y
+#$ -N segCBCTCV6
+#$ -cwd
+hostname
+date
+source /share/apps/source_files/python/python-3.9.5.source
+source /share/apps/source_files/cuda/cuda-11.0.source
+python3 -u train.py \
+--project CBCTUnetSeg \
+--exp_name noaffine_segModeCBCTCV6 \
+--data_path ./Data/fullResCropIntensityClip_resampled \
+--batch_size 8 \
+--input_mode cbct \
+--inc 1 \
+--outc 2 \
+--cv 6 \
+--input_shape 64 101 91 \
+--lr 1e-5 \
+--affine_scale 0.0 \
+--save_frequency 1000 \
+--num_epochs 20000 \
+--w_dce 1.0 \
+--using_HPC 1 \
+--nc_initial 16 \
+--two_stage_sampling 0 \
+                   
+                   
